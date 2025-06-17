1027bef4240170f35a0f75e40b7cbdae85ec2d9b
madhu200569 <chinnichinni1026@gmail.com>
2025-06-17 21:09:33 +0530

    Add files via upload

---
+++ Tabulation.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Tabulation
+{
+    public static void main(String[] args)
+    {
+        int n=5;
+        int[] memo=new int[n+1];
+        memo[1]=1;
+        for(int i=2;i<memo.length;i++)
+        {
+            memo[i]=memo[i-1]+memo[i-2];
+            
+        }
+        System.out.println(memo[n]);
+    }
+}

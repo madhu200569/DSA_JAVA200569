ae0306b2014670176c00b2c924398e53a3552844
madhu200569 <chinnichinni1026@gmail.com>
2025-06-17 21:21:34 +0530

    Add files via upload

---
+++ Maximumarray.java
@@ -0,0 +1,24 @@
+import java.util.*;
+ public class Maximumarray
+{
+    public static void main( String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int [] a=new int[5];
+        for( int i=0;i<a.length;i++)
+        {
+            a[i]=sc.nextInt();
+            
+        } 
+        int m=a[0];
+         for( int i=0;i<a.length;i++)
+         {
+            if(a[i]>m)
+            m=a[i];
+         }
+         System.out.println(m);
+            
+         
+                 
+    }
+}

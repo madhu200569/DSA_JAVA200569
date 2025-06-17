cd228a30b5441dc071b3b0ad011fed46d0b583bd
madhu200569 <chinnichinni1026@gmail.com>
2025-06-17 20:42:08 +0530

    Add files via upload

---
+++ Binarysearch.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class Binarysearch
+{
+	public static void main(String[] args) {
+	    int[] arr=new int[5];
+	    Scanner sc=new Scanner(System.in);
+	    for(int i=0;i<arr.length;i++)
+	    {
+	        arr[i]=sc.nextInt();
+	        
+	    }
+	   int s=sc.nextInt();
+	   int i=0;
+	   int j=arr.length-1;
+	   while(i<1)
+	   {
+	      int mid=(i+j/2);
+	        if(arr[mid]==s)
+	        {
+	            System.out.println(mid);
+	            System.exit(0);
+	        }
+	        if(arr[mid]<s) i=mid+1;
+	        if(arr[mid]>s) j=mid;
+	    }
+	    System.out.println("not found");
+	}
+}

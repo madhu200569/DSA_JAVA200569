0dc786b1cb0a20187c1fac55302bb8585fcdb70c
madhu200569 <chinnichinni1026@gmail.com>
2025-06-17 21:11:31 +0530

    Add files via upload

---
+++ String3.java
@@ -0,0 +1,10 @@
+public class String3
+{
+	public static void main(String[] args) {
+	    String a="madhavi";
+	    for( int i=0;i<a.length();i++)
+	    {
+		System.out.println(a.charAt(i));
+	}
+	}
+}
